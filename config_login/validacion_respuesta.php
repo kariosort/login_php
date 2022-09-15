40822a80e816aadc3e54f5686d2dcbe457a23520
kariosort <karimhernandez233@gmail.com>
2022-09-15 10:44:31 -0600

    ggjhgjgjgjgj

--- config_login/validacion_respuesta.php
+++ config_login/validacion_respuesta.php
@@ -4,11 +4,8 @@ include('conexion.php');
 
 $respuesta=mysqli_real_escape_string($conexion,$_POST['respuesta']);
 $preguntas=mysqli_real_escape_string($conexion,$_POST['preguntas']);
-$usuario=isset($_GET['resultado']);
 
-echo $usuario;
-
-/* $query=("SELECT COUNT(*) as contar FROM preguntas_usuario pu 
+/$query=("SELECT COUNT(*) as contar FROM preguntas_usuario pu 
 inner JOIN usuarios_nueva u ON pu.id_usuario = u.id_usuario 
 inner JOIN preguntas p ON pu.id_pregunta = p.id_pregunta
 WHERE pu.respuesta='$respuesta'and p.pregunta='$preguntas' and u.usuario='josemario'");
@@ -22,5 +19,5 @@ $array=mysqli_fetch_array($consulta);
 
     }else{
         echo 'houston tenemos un problema';
-    }  */
+    }
 ?> 
